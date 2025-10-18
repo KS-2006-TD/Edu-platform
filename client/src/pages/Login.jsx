e0f99543301a9cd38780694d2b56354fa573a325
KS-2006-TD <kiransaipanguluri2k06@gmail.com>
2025-10-18 07:09:14 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
