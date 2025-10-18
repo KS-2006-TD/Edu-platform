f4e08696f9b3eb7d267b46eaf856318d0e1d0666
KS-2006-TD <kiransaipanguluri2k06@gmail.com>
2025-10-18 07:30:19 +0530

    Fix import case for Dashboard.css

--- client/src/pages/StudentDashboard.jsx
+++ client/src/pages/StudentDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Dashboard.css";
+import "./dashboard.css";
 
 export default function StudentDashboard({ user }) {
   const [allCourses, setAllCourses] = useState([]);
