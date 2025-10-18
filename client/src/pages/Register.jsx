e4a88177513da6110beeeaff6d8068b137f32d83
KS-2006-TD <kiransaipanguluri2k06@gmail.com>
2025-10-18 11:52:29 +0530

    Fix import case for register.css

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import './Register.css';
+import './register.css';
 
 export default function Register() {
   const [name, setName] = useState('');
